c0f388111caa675a4c9c09b61c0f24b3d74b4d18
Aurélien Gâteau <agateau@kde.org>
2012-03-02 23:21:07 +0100

    get rid of site prefix

--- postprocess.sh
+++ postprocess.sh
@@ -2,3 +2,4 @@ sed -i 's,\[sourcecode language="\([a-z]*\)"\],{% codeblock lang:\1 %},' *.markd
 sed -i 's,\[sourcecode lang="\([a-z]*\)"\],{% codeblock lang:\1 %},' *.markdown
 sed -i 's,\[/sourcecode\],{% endcodeblock %},' *.markdown
 sed -i 's/^_ /_/' *.markdown
+sed -i 's,http://agateau.files.wordpress.com,,g' *.markdown
