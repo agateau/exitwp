1203fd3f2026483a75bb43616a96c7207aefebad
Aurélien Gâteau <agateau@kde.org>
2012-03-03 14:47:28 +0100

    Replace more urls

--- postprocess.sh
+++ postprocess.sh
@@ -3,3 +3,4 @@ sed -i 's,\[sourcecode lang="\([a-z]*\)"\],{% codeblock lang:\1 %},' *.markdown
 sed -i 's,\[/sourcecode\],{% endcodeblock %},' *.markdown
 sed -i 's/^_ /_/' *.markdown
 sed -i 's,http://agateau.files.wordpress.com,,g' *.markdown
+sed -i 's,http://agateau.wordpress.com,,g' *.markdown
